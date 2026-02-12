6525f7abedeb9ab4ba649b8543f93688a77e50fd
Anurag Chauhan <nanuchauhan2006@gmail.com>
2026-02-12 10:37:15 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,17 @@
+*/: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (the array)
+- Third line: integer pos (1-based position)
+- Fourth line: integer x (element to insert)
+
+Output:
+- Print the updated array (n+1 integers) in a single line, space-separated/*
+
+
+#include<stdio.h>
+int main(){
+    int n ;
+    printf()
+}
